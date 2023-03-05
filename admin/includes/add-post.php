1370e2866d8ca1f55fb567c6346382175ad36050
nghiamd <nghiamd002@gmail.com>
2023-03-05 23:30:20 +0700

    Nghia update code 5-3-2023

--- admin/includes/add-post.php
+++ admin/includes/add-post.php
@@ -1,21 +1,28 @@
 <?php 
     if(isset($_POST['create_post'])){
-        $post_title  = $_POST['title'];
+        $post_title   = $_POST['title'];
         $post_author  = $_POST['author'];
         $post_cat_id  = $_POST['cat_id'];
         $post_status  = $_POST['status'];
-        $post_img = $_FILES['image']['name'];
+        $post_img     = $_FILES['image']['name'];
         $post_img_tmp = $_FILES['image']['tmp_name'];
-        $post_tags  = $_POST['tags'];
-        $post_content  = $_POST['content'];
+        $post_tags    = $_POST['tags'];
+        $post_content = $_POST['content'];
+        $post_date = date('d-m-y');
+        $post_comment_count = 4;
 
         move_uploaded_file($post_img_tmp, "../imgs/$post_img");
+
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comments_count, post_status)";
+        $query.=" VALUES(${post_cat_id}, '{$post_title}', '{$post_author}', now(), '{$post_img}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+
+        $result = mysqli_query($conn, $query);
        
     }
 
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<form form form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
         <input type="text" class="form-control" name="title">
